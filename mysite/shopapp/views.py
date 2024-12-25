bdc9708860231067b2c593300ceff16e812dfb63
inferal <pyurik44@gmail.com>
2024-12-25 15:50:05 +0300

    Конец второго дня по Django

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -5,7 +5,13 @@ from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
     context = {
         "time_running": default_timer(),
+        "products": products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
