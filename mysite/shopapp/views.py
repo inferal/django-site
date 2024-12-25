ea4d699a2f8cebff5e67309ef34a111f1531de76
Yura <pyurik44@gmail.com>
2024-12-25 10:42:29 +0300

    test

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
 
 
-def shop_index(request):
-    return HttpResponse("Hello World")
+def shop_index(request: HttpRequest):
+    context = {
+        "time_running": default_timer(),
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
