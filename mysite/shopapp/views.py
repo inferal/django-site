df92b9e7f50411f3cc39aab3de659eb4483ccd76
inferal <pyurik44@gmail.com>
2024-12-26 14:18:36 +0300

    3.3 Свойства полей

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import Product
 
 def shop_index(request: HttpRequest):
     products = [
@@ -22,3 +23,9 @@ def groups_list(request: HttpRequest):
         "groups": Group.objects.prefetch_related('permissions').all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
+
+def products_list(request: HttpRequest):
+    context ={
+        "products": Product.objects.all(),
+    }
+    return  render(request, 'shopapp/products-list.html', context=context)
