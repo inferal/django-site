e1658ff8553d56eaff7b71b37d5e8a1b9fe7f4ec
inferal <pyurik44@gmail.com>
2024-12-26 16:15:38 +0300

    3.5 Метаданные моделей

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Product(models.Model):
+    class Meta:
+        ordering = ["name", "price"]
+        # db_table = "tech_products"
+        # verbose_name_plural = "products"
+
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
