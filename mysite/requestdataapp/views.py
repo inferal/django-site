fe00ddb14732a78460bbc25c041a908b9fc6edff
yurik <pyurik44@gmail.com>
2025-01-10 23:55:13 +0300

    6.3 Дополнительные возможности валидации форм

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -2,7 +2,7 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .forms import UserBioForm
+from .forms import UserBioForm, UploadFileForm
 
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
@@ -23,13 +23,19 @@ def user_form(request: HttpRequest) -> HttpResponse:
     return render(request, "requestdataapp/user-bio-form.html", context=context)
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    context = {
 
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+    else:
+        form = UploadFileForm()
+
+    context = {
+        "form": form,
     }
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        print("save file", filename)
 
     return render(request, "requestdataapp/file-upload.html", context=context)
