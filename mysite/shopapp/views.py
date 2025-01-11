ed1a0bce23a80383de03bff6449d452beccf56fc
yurik <pyurik44@gmail.com>
2025-01-11 17:07:10 +0300

    6.4 ModelForm

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -35,7 +35,8 @@ def create_product(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            Product.objects.create(**form.cleaned_data)
+            # Product.objects.create(**form.cleaned_data)
+            form.save()
             url = reverse('shopapp:products_list')
             return redirect(url)
     else:
